35b822f643679b64801d8308266561fdb6bc39e7
JMenon <adm7utd@gmail.com>
2022-03-11 23:50:10 +0530

    importing and calling matrix functions

--- rat_maze.py
+++ rat_maze.py
@@ -1,6 +1,5 @@
 
 """
-TODO: put create matrix and get start and end in their own "matrix" module
 TODO: write docstrings (MEDIUM )
 TODO: comment code (MEDIUM )
 
@@ -13,14 +12,15 @@ TODO: test code with multiple input files - including no solution files (HIGH )
 
 from helpers.argument_parser import ArgumentParser as ap
 from helpers.file_ops import FileOps
+from helpers.matrix import Matrix
 from maze_solver.maze_solver import MazeSolver
 
 def main():
     in_path, out_path, algo = ap.take_input()
     infile = FileOps.read_file(in_path)
 
-    matrix = MazeSolver.create_matrix(infile) 
-    start, end = MazeSolver.get_start_and_end(matrix)
+    matrix = Matrix.create_matrix(infile) 
+    start, end = Matrix.get_start_and_end(matrix)
     solved_maze = MazeSolver.solve_maze(start, end, matrix, algo)
 
     FileOps.write_output_file(matrix, solved_maze, out_path)
